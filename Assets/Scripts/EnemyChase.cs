c8e065361d718c6ac031d10c355420d59a188cb9
EmreOzhazer <emreozhazer@hotmail.com>
2023-04-17 19:18:48 +0300

    enemy patrol with animations (not complated yet)

--- Assets/Scripts/EnemyChase.cs
+++ Assets/Scripts/EnemyChase.cs
@@ -11,32 +11,78 @@ public class EnemyChase : MonoBehaviour
     
     public Transform playerToChase;
 
-    private bool isChasing=false;
+    private Animator animator;
+
+    public Transform[] waypoints;
+    private int wayPointIndex;
+    private Vector3 target;
+
+    private bool isChasing = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        UpdateDestionation();
+        animator.SetBool("isWalking",true);
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        
         if (isChasing == true)
         {
             transform.LookAt(playerToChase);
             enemyAgent.SetDestination(playerToChase.transform.position);
         }
+
+        if (Vector3.Distance(transform.position, target) < 1)// less than 1 metre close 
+        {
+           
+            IterateWaypointIndex();
+            UpdateDestionation();
+        }
         
     }
+    
+    //patrol atarken gizmolar nav mesh agent ama gözükürse nav mesh agent arı oluyor.
+    //kovaladıktan sonra eğer arı kaçarsa 2 saniye sonra geri döner patrol atmaya
 
+    void UpdateDestionation()
+    {
+        
+        target = waypoints[wayPointIndex].position;
+        enemyAgent.SetDestination(target);
+    }
+
+    void IterateWaypointIndex()
+    {
+        wayPointIndex++;
+        if (wayPointIndex == waypoints.Length) wayPointIndex = 0;
+    }
     private void OnTriggerEnter(Collider other)
     {
         isChasing = true;
-
+        animator.SetBool("isRunning",true);
+        animator.SetBool("isWalking",false);
     }
 
     private void OnTriggerExit(Collider other)
     {
         isChasing = false;
+        //animator.SetBool("isRunning",false);
+       StartCoroutine(backToPatrol());
+       
+        IEnumerator backToPatrol()
+        {
+            
+            
+            yield return new WaitForSeconds(2);
+            UpdateDestionation();
+           
+        }
+        //animator.SetBool("isMoving",false);
     }
+    
 }
